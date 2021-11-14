e17c02a5db229eb0b8f099cd32ac24d6534eb176
Sharon-300 <83770248+Sharon-300@users.noreply.github.com>
2021-11-14 11:49:36 +0530

    Add files via upload

---
+++ Week-1/Day 1/nth node from end.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+#include "Linked_List.h"
+using namespace std;
+
+int main()
+{
+	class LL obj;//object for class in header file
+	
+	/*creating a linked list*/
+	obj.Insert(5);
+	obj.Insert(1);
+	obj.Insert(12);
+	obj.Insert(51);
+	obj.Insert(7);
+	obj.Insert(3);
+	
+	int n;
+	cout<<"Enter the value of n for the nth node from the end: ";
+	cin>>n;
+	
+	int len=obj.length();//finding length of linked list
+	
+	if(n<1||n>len)	cout<<"\nInvalid input"<<endl;//checking conditions for n value
+	
+	else
+	{
+		int req=len-n+1;//nth node from end will be (len-n+1)th node from front
+		cout<<"Element at position "<<n<<" from the end is "<<obj.Element(req);
+	}
+}
